763e929c5b0166b5ceb0e7bb24d218b26d25a247
nileetjacobphilip <73283503+nileetjacobphilip@users.noreply.github.com>
2021-03-14 23:48:32 +0000

    Code Refactor and Tile Image drawing
    Code Refactoring done for the entire game.j javascript File
    
    Finished the for-loop for DrawImage Functionality
    
    For each loop to set the images with Set attributes
    
    Handles exceptions in the code block to check the log error

--- js/game.js
+++ js/game.js
@@ -59,6 +59,7 @@
                 image.src = URL.createObjectURL(img);
                 image.addEventListener('load', () => creatImagePieces(image));
                 setInterval(() => checkDistance(), 1000);
+                console.log(puzzle);
             })
             .catch((error)=> {console.log('takePhoto() error', error)});
     };
@@ -68,10 +69,24 @@
         const ctx = canvas.getContext('2d');
         const pieceWidth = image.width/numCol;
         const pieceHeight = image.height/numRow;
+        //forloop Function to Draw the images on each pieces
         for(let x=0; x < numCol; ++x){
             for(let y=0; y<numRow; ++y){
                 ctx.drawImage(image, x * pieceWidth, y * pieceHeight, pieceWidth,pieceHeight,0,0, canvas.width,canvas.height);
+                imgPieces[8-pieces] = canvas.toDataURL("image/png").replace("image/png","image/octet-stream");
+                console.log(imgPieces);
+                pieces= pieces-3;
+                if (pieces<0) {
+                    pieces= (puzzlePieces-1)+pieces;
+                }
             }
-        }
+        };
+        markers.forEach((marker,i) =>{
+            const aImg = document.createElement("a-image");
+            aImg.setAttribute('rotation','-90 0 0')
+            aImg.setAttribute('position','0 0 0')
+            aImg.setAttribute('src',imgPieces[puzzle[i]]);
+            marker.appendChild(aImg);
+        })
     }
 }
